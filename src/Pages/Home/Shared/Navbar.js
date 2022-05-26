953b17eee642c3a9232bad5e6ee99bb78f28328f
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-26 18:02:09 +0600

    banner added

--- src/Pages/Home/Shared/Navbar.js
+++ src/Pages/Home/Shared/Navbar.js
@@ -2,30 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const menuItems = <>
+        <li><Link to={"/"}>Home</Link></li>
+        <li><Link to={"/photos"}>Photos</Link></li>
+        <li><Link to={"/profile"}>Profile</Link></li>
+        <li><Link to={"/contact"}>Contact us</Link></li>
+        <li><Link to={"/about"}>About</Link></li>
+        <li><Link to={"/login"}>Login</Link></li>
+    </>
     return (
-        <div class="navbar bg-secondary text-white">
-            <div class="navbar-start">
-                
-                <Link to="/" class="btn btn-ghost normal-case text-xl">Friends-16</Link>
+        <div className="navbar bg-secondary text-white">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost normal-case text-xl">Friends-Forever</a>
             </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal p-0">
-                    <li><Link to="/photos">Photos</Link></li>
-                    <li tabindex="0">
-                        <Link to="/">
-                            Gang-16
-                            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </Link>
-                        <ul class="p-2">
-                            <li><Link to="/student">Student's Profile</Link></li>
-                            <li><Link to="/teacher">Teacher's Profile</Link></li>
-                        </ul>
-                    </li>
-                    <li><Link to="/private">Private</Link></li>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {menuItems}
                 </ul>
             </div>
-            <div class="navbar-end">
-                <Link to="/login" class="btn">Click Me</Link>
+            <div className="navbar-end">
+                <label tabIndex="1" for="dashboard-sidebar" className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+
             </div>
         </div>
     );
