99db9d074a95481ed0b07cb27a5a3ea08c742fbe
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-27 08:30:39 +0600

    lsgh

---
+++ public/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDw-vW_lqHVTosMr47bvP1qzMMJ9c2N0Ig",
+  authDomain: "friends-forever-31234.firebaseapp.com",
+  projectId: "friends-forever-31234",
+  storageBucket: "friends-forever-31234.appspot.com",
+  messagingSenderId: "939985845335",
+  appId: "1:939985845335:web:8e393d324217ddc25ef712"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
